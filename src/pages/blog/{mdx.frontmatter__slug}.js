ece28f1f65d50e3ae5b60169698f2ae640f6f12c
h-fahad <fahadyounas10@gmail.com>
2023-07-14 00:07:02 +0500

    added hero image sharp

--- src/pages/blog/{mdx.frontmatter__slug}.js
+++ src/pages/blog/{mdx.frontmatter__slug}.js
@@ -2,11 +2,20 @@ import * as React from 'react'
 import { graphql } from 'gatsby'
 import Layout from '../../components/Layout'
 import Seo from '../../components/seo'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 
 const BlogPost = ({ data, children }) => {
+  const image = getImage(data.mdx.frontmatter.hero_image)
   return (
     <Layout pageTitle={data.mdx.frontmatter.title}>
       <p>{data.mdx.frontmatter.date}</p>
+      <GatsbyImage alt={data.mdx.frontmatter.hero_image_alt} image={image} />
+      <p>
+        Photo Credit:{" "}
+        <a href={data.mdx.frontmatter.hero_image_credit_link}>
+          {data.mdx.frontmatter.hero_image_credit_link}
+        </a>
+      </p>
       {children}
     </Layout>
   )
@@ -14,12 +23,20 @@ const BlogPost = ({ data, children }) => {
 
 export const query = graphql`
   query ($id: String) {
-    mdx(id: {eq: $id}) {
-      frontmatter {
-        title
-        date(formatString: "MMMM D, YYYY")
+      mdx(id: {eq: $id}) {
+        frontmatter {
+          title
+          date(formatString: "MMMM D,YYYY")
+          hero_image_alt
+          hero_image_credit_link
+          hero_image_credit_text
+          hero_image {
+            childImageSharp {
+              gatsbyImageData
+            }
+          }
+        }
       }
-    }
   }
 `
 
